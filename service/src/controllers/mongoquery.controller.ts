37cc0c06c0680fd74d6079ac285f665fb2f1398f
Soloydenko-C <Igor.Soloydenko@seattle.gov>
2018-02-15 14:27:27 -0800

    Implement basic mongodb querying

--- service/src/controllers/mongoquery.controller.ts
+++ service/src/controllers/mongoquery.controller.ts
@@ -1,12 +1,26 @@
 import { Request, Response, Router } from 'express';
+import { MongoClient, Db } from 'mongodb';
+
+let database: Db;
+MongoClient
+  .connect('mongodb://localhost:27017/')
+  .then(mongoClient => database = mongoClient.db('myTestDb'))
+  .catch(error => console.log(error));
 
 const router = Router();
 
 router
-  .get('/', (req: Request, res: Response) => {
-    const query = req.query.q;
+  .get('/', async (request: Request, response: Response) => {
+    const queryObject = JSON.parse(request.query.q);
 
-    res.send(`Hello ${query}`);
+    const foundEntreis = await database
+      .collection('myLogCollection')
+      .find(queryObject)
+      .toArray();
+
+    console.info(`Request ${ JSON.stringify(queryObject) } results in ${ foundEntreis.length } entr${ foundEntreis.length === 1 ? 'y' : 'ies' }.`);
+
+    response.status(200).send(foundEntreis);
   })
 
 export const MongoQueryController: Router = router;
