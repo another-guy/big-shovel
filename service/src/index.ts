9514ddcb469b24bb74943be34b8cc8c1257af7ee
Soloydenko-C <Igor.Soloydenko@seattle.gov>
2018-02-15 14:36:31 -0800

    Enable CORS on express.js side

--- service/src/index.ts
+++ service/src/index.ts
@@ -2,11 +2,19 @@ import * as express from 'express';
 
 import { MongoQueryController } from './controllers/mongoquery.controller';
 
-const app: express.Application = express();
 const port = (process.env.PORT || 3000) as number;
 
-app.use('/mongo', MongoQueryController);
-app.use('/', (request, response) => { response.status(200).send('Up-n-running'); });
-
 const timestamp = new Date();
-app.listen(port, () => console.log(`Listening at http://localhost:${port}/ since ${timestamp.toLocaleString()} (Local Time)`));
+
+express()
+  // CORS
+  .use((request, response, nextFunction) => {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    nextFunction();
+  })
+  // Add controllers
+  .use('/mongo', MongoQueryController)
+  .use('/', (request, response) => { response.status(200).send('Up-n-running'); })
+  // Start
+  .listen(port, () => console.log(`Listening at http://localhost:${port}/ since ${timestamp.toLocaleString()} (Local Time)`));
