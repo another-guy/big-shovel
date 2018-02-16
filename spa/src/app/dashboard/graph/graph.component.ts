8814c58381bd2d13e95fbd9328cfca47b97c4e28
Soloydenko-C <Igor.Soloydenko@seattle.gov>
2018-02-16 14:35:13 -0800

    Show something more meaningful

--- spa/src/app/dashboard/graph/graph.component.ts
+++ spa/src/app/dashboard/graph/graph.component.ts
@@ -4,6 +4,7 @@ import * as d3 from 'd3';
 
 import { AppState } from '../../app.reducers';
 import { LogEntry } from '../models/log-entry';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-graph',
@@ -18,14 +19,18 @@ export class GraphComponent {
     return logEntry._id;
   }
   
+  @Input() svgSize = { w: 600, h: 100, };
+
   @Input() logDbQueryRepresentation: string;
   logEntries$: Store<LogEntry[]>;
+  totalLogEntryCount$: Observable<number>;
 
   constructor(
     private _store: Store<AppState>,
     private _viewContainerRef: ViewContainerRef,
   ) {
     this.logEntries$ = _store.select(state => state.dashboard.allLogs[this.logDbQueryRepresentation]);
+    this.totalLogEntryCount$ = this.logEntries$.map(logEntries => logEntries.length);
   }
 
   ngOnInit(): void {
@@ -37,34 +42,35 @@ export class GraphComponent {
   }
 
   private plot(logEntries: LogEntry[]): void {
-    const svgWidth = 600;
-    const svgHeight = 100;
-
-    const hourlyGrouped = this.groupByHours(logEntries);
-    const byHourEntryCounts = d3.keys(hourlyGrouped).map(prop => hourlyGrouped[prop].length);
-
-    const yScale = d3.scaleLinear()
-      .domain([ 0, d3.max(byHourEntryCounts) ])
-      .range([ 0, svgHeight ]);
-    const yScaleReverse = d3.scaleLinear()
-      .domain(yScale.domain())
-      .range(yScale.range().reverse())
-
-    const hourlyLogEntryCounts = [
-      '00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11',
-      '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23',
-    ].map(hour => {
-      const logEntriesForHour = hourlyGrouped[hour] || [];
-      return {
-        errors: logEntriesForHour.filter(logEntry => logEntry.level === 'ERROR').length,
-        infos: logEntriesForHour.filter(logEntry => logEntry.level === 'INFO').length,
-      }
-    });
 
     const rootElement = this._viewContainerRef.element.nativeElement;
     
     const barPadding = 3;
     const base = 15;
+
+    const svgWidth = this.svgSize.w;
+    const svgHeight = this.svgSize.h;
+
+    const dateAndHourlyGrouped = this.groupByHours(logEntries);
+    const dateAndHourGroupCounts = d3.keys(dateAndHourlyGrouped).map(prop => dateAndHourlyGrouped[prop].length);
+
+    const yScale = d3.scaleLinear()
+      .domain([ 0, d3.max(dateAndHourGroupCounts) ])
+      .range([ 0, svgHeight - base * 2 ]);
+    const yScaleReverse = d3.scaleLinear()
+      .domain(yScale.domain())
+      .range(yScale.range().reverse());
+
+    const dateAndHourlyLogEntryCounts = d3.keys(dateAndHourlyGrouped)
+      .map(dateAndHour => {
+        const logEntriesForDateAndHour = dateAndHourlyGrouped[dateAndHour] || [];
+        return {
+          errors: logEntriesForDateAndHour.filter(logEntry => logEntry.level === 'ERROR').length,
+          infos: logEntriesForDateAndHour.filter(logEntry => logEntry.level === 'INFO').length,
+        }
+      });
+
+    const sumAll = (count: { errors: number, infos: number }) => count.errors + count.infos;
 
     const svg = d3.select(rootElement)
       .select('svg')
@@ -72,55 +78,57 @@ export class GraphComponent {
         .attr('height', svgHeight);
 
     svg.selectAll('rect.error-segment')
-      .data(hourlyLogEntryCounts)
+      .data(dateAndHourlyLogEntryCounts)
       .enter()
       .append('rect')
         .attr('class', 'error-segment')
-        .attr('fill', count => `rgb(${ 256 - count.errors * 2 }, 0, 0)`)
-        .attr('x', (_, index) => index * (svgWidth / hourlyLogEntryCounts.length - barPadding))
-        .attr('y', (count, _) => svgHeight - yScale(count.errors) - base)
+        .attr('fill', count => `#ccc`)
+        .attr('x', (_, index) => index * (svgWidth / dateAndHourlyLogEntryCounts.length - barPadding))
+        .attr('y', (count, _) => svgHeight - yScale(sumAll(count)) - base)
         .attr('width', 20)
-        .attr('height', count => yScale(count.errors));
+        .attr('height', count => yScale(sumAll(count)));
     
     svg.selectAll('text.errorBar')
-      .data(hourlyLogEntryCounts)
+      .data(dateAndHourlyLogEntryCounts)
       .enter()
       .append('text')
         .attr('class', 'errorBar')
         .attr('fill', 'red')
-        .attr('x', (_, index) => 1.5 + index * (svgWidth / hourlyLogEntryCounts.length - barPadding))
-        .attr('y', count => svgHeight - yScale(count.errors) - 2 - base)
-        .text(count => +count.errors || '');
+        .attr('x', (_, index) => 1.5 + index * (svgWidth / dateAndHourlyLogEntryCounts.length - barPadding))
+        .attr('y', count => svgHeight - yScale(sumAll(count)) - 2 - base)
+        .text(count => +sumAll(count) || '');
 
+    const keys = d3.keys(dateAndHourlyGrouped);
     svg.selectAll('text.subs')
-      .data(hourlyLogEntryCounts)
-      .enter()
+      .data(keys)
+        .enter()
       .append('text')
-        .attr('class', 'subs')
+          .attr('class', 'subs')
         .attr('fill', 'black')
-        .attr('x', (_, index) => 1.5 + index * (svgWidth / hourlyLogEntryCounts.length - barPadding))
-        .attr('y', count => svgHeight)
-        .text((_, index) => ('0' + index).slice(-2));
+        .attr('x', (_, index) => 1.5 + index * (svgWidth / dateAndHourlyLogEntryCounts.length - barPadding))
+          .attr('y', count => svgHeight)
+        .text((_, index) => keys[index]);
+
 
     const yAxis = d3.axisRight(yScaleReverse).ticks(5);
 
     svg.append('g')
       .attr('class', 'axis')
-      .attr('transform', `translate(${ svgWidth - 25 }, ${ -base })`)
+      .attr('transform', `translate(${ svgWidth - 25 }, ${ base })`)
       .call(yAxis);
   }
 
   private groupByHours(logEntries: LogEntry[]): { [_: string]: LogEntry[] } {
-    const getHourKey = (logEntry: LogEntry): string => logEntry.time.split(' ')[1].split(':')[0];
+    const getPeriodKey = (logEntry: LogEntry): string => logEntry.time.split(':')[0];
 
     return logEntries.reduce(
         (subresult, logEntry) => {
-          const hour = getHourKey(logEntry);
-          const hourData = subresult[hour];
-          if (hourData) {
-            hourData.push(logEntry);
+          const periodKey = getPeriodKey(logEntry);
+          const periodLogEntries = subresult[periodKey];
+          if (periodLogEntries) {
+            periodLogEntries.push(logEntry);
           } else {
-            subresult[hour] = [ logEntry ];
+            subresult[periodKey] = [ logEntry ];
           };
           return subresult;
         },
