9d881221df41df5e81ad491bcfb60f27f1467f7d
Igor Soloydenko <soloydenko@gmail.com>
2018-02-23 22:01:51 -0800

    Add aggregation pipeline example via tooltip

--- spa/src/app/dashboard/log-entries/log-entries.component.ts
+++ spa/src/app/dashboard/log-entries/log-entries.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-log-entries',
   templateUrl: './log-entries.component.html',
-  styleUrls: ['./log-entries.component.css']
+  styleUrls: ['./log-entries.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LogEntriesComponent implements OnInit {
+export class LogEntriesComponent {
 
-  constructor() { }
+  sample =
+`[
+  { "$match": { "level": "ERROR" } },
+  { "$group": { "_id": "$payload.employeeId", "count": { "$sum": 1 } } },
+  {  "$sort": { "_id": 1 } }
+]`;
 
-  ngOnInit() {
+  execute(): void {
   }
 
 }
